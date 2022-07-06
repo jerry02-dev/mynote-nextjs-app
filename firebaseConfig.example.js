99fb798900fbf5100a8fd0d9eb36c4b3e5191b72
jerry-dev <jerrysaydoquisjr@gmail.com>
2022-07-06 15:44:03 +0800

    updated

---
+++ firebaseConfig.example.js
@@ -0,0 +1,15 @@
+import { initializeApp, firebase } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+export const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
